a7d2cc2850db27799fb07777fbe7e3746ebd2d82
rajbhoopathi <62846675+rajbhoopathi@users.noreply.github.com>
2020-04-13 09:01:11 +0530

    added all files

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('hello world')
+
+
+def new(request):
+    return HttpResponse('new products')
